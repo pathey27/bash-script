0f4343b4c1176122d98a52b02c153aa99a88b3f3
pathey27 <patheymistry98@gmail.com>
2026-04-29 06:00:44 +0000

    add system info script

---
+++ firstscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+### this script prints system info ###
+
+echo "welcome to bash script"
+echo
+
+echo "###################################"
+echo "the uptime of the system is "
+
+uptime
+echo "###################################"
+echo "memory utilization"
+free -m
+
+
+echo "###################################"
+echo "disk utilize"
+df -h
+
